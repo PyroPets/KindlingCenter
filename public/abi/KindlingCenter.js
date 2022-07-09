23e6a7ffa34655735d037d0ee3f3ca1664f6a936
SeqSEE <sarah@sqdmc.net>
2022-07-09 03:38:43 -0400

    add static site

---
+++ public/abi/KindlingCenter.js
@@ -0,0 +1,78 @@
+const abi = [
+  {
+    inputs: [
+      {
+        internalType: 'contract PyroBase',
+        name: '_base',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'constructor',
+  },
+  {
+    inputs: [],
+    name: 'base',
+    outputs: [
+      {
+        internalType: 'contract PyroBase',
+        name: '',
+        type: 'address',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'playAll',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'playAllForAddr',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'generation',
+        type: 'uint256',
+      },
+    ],
+    name: 'playGeneration',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: 'generation',
+        type: 'uint256',
+      },
+      {
+        internalType: 'address',
+        name: 'owner',
+        type: 'address',
+      },
+    ],
+    name: 'playGenerationForAddr',
+    outputs: [],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  },
+];
+export default abi;
---
+++ public/contracts/deployed.js
@@ -0,0 +1,11 @@
+const contracts = {
+  MainNet: {
+    PyroCore: '848b9ec4299dac31d21c5db87add813d0cfc58c5',
+    KindlingCenter: 'ca3a3ab59542ffb2fe54274f3d79fd3963153e73',
+  },
+  TestNet: {
+    PyroCore: '0000000000000000000000000000000000000000',
+    KindlingCenter: '0000000000000000000000000000000000000000',
+  },
+};
+export default contracts;
